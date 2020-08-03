2301bbf4ca89cd76c01a65b978ae788a5c5204db
nurww <nurdaule@gmail.com>
2020-08-04 01:06:28 +0600

    change folder name dist --> docs

--- webpack.config.js
+++ webpack.config.js
@@ -3,92 +3,95 @@ const autoprefixer = require('autoprefixer');
 const precss = require('precss');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-// console.log(path.resolve(__dirname, 'src'),'kokopkpkokopkpkokopkpkokopkpkokopkp');
+const { config } = require('process');
 
 module.exports = {
-  entry: {
-    app: './js/app.js',
-  },
-  context: path.resolve(__dirname, 'src'),
-  devServer: {
-    publicPath: '/',
-    port: 9000,
-    contentBase: path.join(process.cwd(), 'docs'),
-    host: 'localhost',
-    historyApiFallback: true,
-    noInfo: false,
-    stats: 'minimal',
-    hot: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.pug$/,
-        loader: 'pug-loader',
-        options: {
-          pretty: true
-        }
-      },
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          MiniCssExtractPlugin.loader,
-          {
-            loader: 'css-loader',
-            options: { sourceMap: true }
-          }, {
-            loader: 'postcss-loader',
-            options: { sourceMap: true, config: { path: 'src/js/postcss.config.js' } }
-          }
-        ]
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          'style-loader',
-          MiniCssExtractPlugin.loader,
-          {
-            loader: 'css-loader',
-            options: { sourceMap: true }
-          }, {
-            loader: 'postcss-loader',
-            options: { sourceMap: true, config: { path: 'src/js/postcss.config.js' } }
-          }, {
-            loader: 'sass-loader',
-            options: { sourceMap: true}
-          }
-        ]
-      },
-      {
-        test: /\.(png|jpe?g|gif|svg)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[path][name].[ext]',
-            },
-          },
-        ],
-      },
-      {
-        test: /\.(eot|ttf|woff|woff2|otf)$/,
-        loader: 'file-loader',
-        options: {
-          name: './fonts/[name].[ext]'
-        }
-      }
-    ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'index.pug',
-    }),
-    new MiniCssExtractPlugin({ filename: './main.css' })
-  ],
-  output: {
-    path: path.resolve(__dirname, 'docs'),
-    filename: '[name].js',
-  },
-  mode: 'development'
-}
+	entry: {
+		app: './js/app.js',
+	},
+	context: path.resolve(__dirname, 'src'),
+	devServer: {
+		publicPath: '/',
+		port: 9000,
+		contentBase: path.join(process.cwd(), 'docs'),
+		host: 'localhost',
+		historyApiFallback: true,
+		noInfo: false,
+		stats: 'minimal',
+		hot: true,
+	},
+	module: {
+		rules: [
+			{
+				test: /\.pug$/,
+				loader: 'pug-loader',
+				options: {
+					pretty: true,
+				},
+			},
+			{
+				test: /\.css$/,
+				use: [
+					'style-loader',
+					MiniCssExtractPlugin.loader,
+					{
+						loader: 'css-loader',
+						options: { sourceMap: true },
+					},
+					{
+						loader: 'postcss-loader',
+						options: { sourceMap: true, config: { path: 'src/js/postcss.config.js' } },
+					},
+				],
+			},
+			{
+				test: /\.scss$/,
+				use: [
+					'style-loader',
+					MiniCssExtractPlugin.loader,
+					{
+						loader: 'css-loader',
+						options: { sourceMap: true },
+					},
+					{
+						loader: 'postcss-loader',
+						options: { sourceMap: true, config: { path: 'src/js/postcss.config.js' } },
+					},
+					{
+						loader: 'sass-loader',
+						options: { sourceMap: true },
+					},
+				],
+			},
+			{
+				test: /\.(png|jpe?g|gif|svg)$/,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							name: '[path][name].[ext]',
+						},
+					},
+				],
+			},
+			{
+				test: /\.(eot|ttf|woff|woff2|otf)$/,
+				loader: 'file-loader',
+				options: {
+					name: './fonts/[name].[ext]',
+				},
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: 'index.pug',
+		}),
+		new MiniCssExtractPlugin({ filename: './main.css' }),
+	],
+	output: {
+		path: path.resolve(__dirname, 'docs'),
+		filename: '[name].js',
+	},
+	mode: 'development',
+};
